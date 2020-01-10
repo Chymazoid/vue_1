6dd43d77545c31b27cfb705a189bb4fd6d34403e
d.drugov <d.drugov@it.ecommpay.com>
2020-01-10 10:37:25 +0300

    resource

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <template>
-    <div>
-       <h2 >{{title}}</h2>
+    <div class="container pt-2">
+        <div class="form-group">
+            <label for="name">Car Name</label>
+            <input type="text" id="name" class="form-control" v-model.trim="carName">
+        </div>
+
+        <div class="form-group">
+            <label for="year">Car Year</label>
+            <input type="text" id="year" class="form-control" v-model.number="carYear">
+        </div>
+
+        <button class="btn btn-success" @click="createCar">Create car</button>
+        <button class="btn btn-primary" @click="loadCars">Load cars</button>
+        <hr>
+        <ul class="list-group">
+            <li
+                class="list-group-item"
+                v-for="car of cars"
+                :key="car.id"
+            >
+                <strong>{{car.name}}</strong> - {{car.year}}
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -11,11 +32,49 @@
     export default {
         data() {
             return {
-                title: 'Hi i am',
-                names: ['vald', 'max','Elena']
-
+                carName: '',
+                carYear: 2018,
+                cars: [],
+                resource: null
             }
+        },
+        methods : {
+            createCar() {
 
+                const car = {
+                    name: this.carName,
+                    year: this.carYear
+                }
+            /*  this.$http.post('http://localhost:3000/cars', car)
+              .then(response=> {
+                  return response.json()
+              })
+              .then(newCar => {
+                  console.log(newCar)
+              })*/
+
+                this.resource.save({}, car);
+            },
+            loadCars() {
+                /*this.$http.get('http://localhost:3000/cars')
+                    .then(response=> {
+                        return response.json()
+                    })
+                    .then(cars=>{
+                        this.cars= cars
+                })*/
+
+                this.resource.get()
+                    .then(response=> {
+                        return response.json()
+                    })
+                    .then(cars=>{
+                        this.cars= cars
+                })
+            }
+        },
+        created() {
+            this.resource = this.$resource('cars')
         }
 
     }
